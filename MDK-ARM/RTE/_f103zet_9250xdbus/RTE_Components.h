5994109019aac9e6060e5748b8b96f1cc284769f
Wmd <982670455@qq.com>
2018-06-15 19:41:48 +0800

    第一次提交 初始化了外设端口

---
+++ MDK-ARM/RTE/_f103zet_9250xdbus/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'f103zet_9250xdbus' 
+ * Target:  'f103zet_9250xdbus' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
